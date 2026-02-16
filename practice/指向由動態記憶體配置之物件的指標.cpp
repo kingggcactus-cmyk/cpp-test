de49f2d910e7388955bafe12f3758bf6e80bab11
Wang ting kai <kingggcactus@gmail.com>
2026-02-16 15:42:15 +0800

    commit and push

---
+++ practice/指向由動態記憶體配置之物件的指標.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Cwin{
+    protected:
+      char id;
+      int width, height;
+    public:
+      Cwin(char i = 'D', int w = 10, int h = 10)
+      {
+        id = i;
+        width = w;
+        height = h;
+      }
+      void show_area(void)
+      {
+        cout << "window " << id << ", area= " << area() << endl;
+      }
+      virtual int area(void)
+      {
+        return height*width;
+      }
+};
+
+class CMiniWin : public Cwin{
+    public:
+      CMiniWin(char i, int w, int h):Cwin(i, w, h){}
+
+      virtual int area(void)
+      {
+        return (int)(0.8*width*height);
+      }
+};
+
+int main()
+{
+    Cwin *ptr = new Cwin('A', 70, 80);
+    CMiniWin m_win('B', 50, 60);
+    ptr->show_area();
+    delete ptr;
+    ptr = &m_win;
+    ptr->show_area();
+    return 0;
+}
