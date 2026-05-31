7e1d4879a84a5e391f07677b428264cf8d8ab817
Wang ting kai <kingggcactus@gmail.com>
2026-05-31 20:21:30 +0800

    commit and push

---
+++ practice/chatgpt隨便出題.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    string str[20];
+    cin >> str[];
+    int count;
+    for(int i = 0; i < str; i++)
+    {
+        if(str[i]==str[i+1]) count++;
+    }
+    return 0;
+
+}
